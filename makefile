ceb54ae4f342d0ed2397609b45c6bdae85bf5c72
Peter Anselmo <peter_anselmo@yahoo.com>
2013-04-21 20:45:41 -0400

    no source changes, just fresh compilation

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC = g++ --std=c++0x -g -O0 -Wall #debugging
 #CC = nvcc --std=c++0x -O3 -Wall
-NVCC = nvcc
+NVCC = nvcc -O3
 all:ssa
 #tests:tests.cpp read.cu sw_matrix.cpp contig.cu fastqfile.cu fasta.cu assembly.cu samfile.cpp
 #	$(CC) -o tests tests.cpp
--- ssa.cu
+++ ssa.cu
@@ -6,8 +6,8 @@
 using namespace std;
 
 char outpath[] = "asdf.txt";
-//char inpath[] = "/home/audioman/Storage/BioInfo/reads/glados_758/phix_10k.fastq";
-char inpath[] = "/home/audioman/Storage/BioInfo/reads/illumina_phix/phix_10_by_hand.fastq";
+char inpath[] = "/home/audioman/Storage/BioInfo/reads/glados_758/phix_100k.fastq";
+//char inpath[] = "/home/audioman/Storage/BioInfo/reads/illumina_phix/phix_10_by_hand.fastq";
 string fastapath = "out.fasta";
 
 int main(int argc, char* argv[]){
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]){
     Assembly assem(fastq);
     assem.assemble_perfect_contigs();
     assem.trim_contigs();
-    assem.assemble_contigs();
+//    assem.assemble_contigs();
     assem.print_contigs();
 
 
